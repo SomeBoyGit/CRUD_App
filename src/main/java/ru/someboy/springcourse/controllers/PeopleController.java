0dfa868eca71d74123ac462c52d5385a5b1cd9ef
someboygit <someboygit@gmail.com>
2022-08-21 13:47:38 +0300

    Add validation name, age and email. Validation it's work!)

--- src/main/java/ru/someboy/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/someboy/springcourse/controllers/PeopleController.java
@@ -1,12 +1,13 @@
 package ru.someboy.springcourse.controllers;
 
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.someboy.springcourse.dao.PersonDAO;
 import ru.someboy.springcourse.models.Person;
+
+import javax.validation.Valid;
 
 /**
  * @author Slipets Artem
